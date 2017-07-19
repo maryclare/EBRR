c9ae29e091df04bee2e402ddda84da3a7488c429
Maryclare Griffin <maryclaregriffin@gmail.com>
2017-07-19 15:35:06 -0400

    Fixed to use new method

--- R/estRegPars.R
+++ R/estRegPars.R
@@ -1,3 +1,26 @@
+fq <- function(q, kurt) {
+  gamma(5/q)*gamma(1/q)/(gamma(3/q)^2) - kurt
+}
+
+fpq <- function(q) {
+  (gamma(5/q)*gamma(1/q)/(q^2*gamma(3/q)^2))*(6*digamma(3/q) - digamma(1/q) - 5*digamma(5/q))
+}
+
+# Use Newton's method: https://en.wikipedia.org/wiki/Newton%27s_method
+nrq <- function(kurt, sval = 0.032, tol = 10^(-12)) { # This starting value is the lowest possible
+  if (kurt < 6) {
+    sval <- 1
+  } else if (kurt < 3) {
+    sval <- 2
+  }
+  x.old <- Inf
+  x.new <- sval
+  while (abs(x.new - x.old) > tol) {
+    x.old <- x.new
+    x.new <- x.old - fq(x.old, kurt)/fpq(x.old)
+  }
+  return(x.new)
+}
 #' Function for estimating tuning parameters
 #'
 #' \code{estRegPars}
@@ -62,13 +85,27 @@ estRegPars <-function(y, X, W = NULL, delta = 0, precomp = NULL) {
   sigma.beta.sq.hat <- sig.2.ests[1]
   sigma.epsi.sq.hat <- sig.2.ests[2]
 
-  aa <- sum(3*(diag(XtHXDDXtHX)*sigma.beta.sq.hat + diag(DXtHXD)*sigma.epsi.sq.hat)^2/p)
-  bb <- sum(diag(DXtHX^4))/p
-  kappa.hat <- (mean(b^4) - aa)/(bb*sigma.beta.sq.hat^2)
+  alpha.beta <- sum(diag(crossprod(DXtHX)))/p
+  gamma.beta <- sum(diag(DXtHX^4))/p
+  omega.beta <- 3*(sum(diag(crossprod(DXtHX)^2)) - sum(diag(DXtHX^4)))/p
+
+  b <- crossprod(HXD, y)
+
+  naive <- (mean(b^4))/(mean(b^2)^2)
+
+  kappa.hat <- (alpha.beta^2/gamma.beta)*(test.stat - omega.beta/alpha.beta^2)
+  q.hat <- nrq(kappa.hat)
+
+  # Old "unbiased"
+  # aa <- sum(3*(diag(XtHXDDXtHX)*sigma.beta.sq.hat + diag(DXtHXD)*sigma.epsi.sq.hat)^2/p)
+  # bb <- sum(diag(DXtHX^4))/p
+  # kappa.hat <- (mean(b^4) - aa)/(bb*sigma.beta.sq.hat^2)
 
   return(list = c("sigma.beta.sq.hat" = sigma.beta.sq.hat,
                   "sigma.epsi.sq.hat" = sigma.epsi.sq.hat,
-                  "kappa.hat" = kappa.hat))
+                  "kappa.hat" = kappa.hat,
+                  "q.hat" = q.hat,
+                  "test.stat" = test.stat))
 
 }
 
